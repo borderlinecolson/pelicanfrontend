bbc29e4a5b9375e5c1480322ab87d48ea6123a0c
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-11 09:21:37 -0400

    Added comments to remind myself of work

---
+++ Pelican/Web/Backend/Auth.php
@@ -0,0 +1,12 @@
+<?php
+
+#Check if email is in Logins.encrypted
+
+#---
+
+#Check if password matchs hash
+
+#---
+
+
+?>
---
+++ Pelican/Web/Backend/Signup.php
@@ -0,0 +1,25 @@
+<php
+
+#Check if email is valid
+
+#---
+
+
+#Check if password is valid
+
+#---
+
+
+#Check if username is taken
+
+#---
+
+#hash details
+
+#---
+
+#Put encrypted details into Logins.encrypted
+
+#---
+
+?>
