c5c92964d6e4746826cf0ca7daa3232ce0618563
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-11 13:22:44 -0400

    Finish later, had to leave

--- Pelican/Web/Backend/Auth/Signup.php
+++ Pelican/Web/Backend/Auth/Signup.php
@@ -3,9 +3,14 @@ $AuthFile="../../../../Logins.encrypted"
 AllowedUsernameChracters=["q","w","e",'r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0']
 
 
+$email = test_input($_POST["email"]);
+
+$username=preg_replace("/[^A-Za-z0-9 ]/", '', $_POST["username"]);
+
+$password=$_POST["password"];
 
 #Check if email is valid
-$email = test_input($_POST["email"]);
+
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
   $emailErr = "Invalid email format";
 }
@@ -14,6 +19,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
 
 #Check if username is allowed
+
 
 
 foreach(file($AuthFile) as $line) {
