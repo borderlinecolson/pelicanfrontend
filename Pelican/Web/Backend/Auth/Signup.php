4fa6586282dbcb19bccf05c9d26f35df63b653ca
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-11 13:08:51 -0400

    Simple changes, need to add username val

--- Pelican/Web/Backend/Auth/Signup.php
+++ Pelican/Web/Backend/Auth/Signup.php
@@ -1,5 +1,7 @@
 <?php
 $AuthFile="../../../../Logins.encrypted"
+AllowedUsernameChracters=["q","w","e",'r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0']
+
 
 
 #Check if email is valid
@@ -11,16 +13,13 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
 
 
-#Check if username is taken
+#Check if username is allowed
+
+
 foreach(file($AuthFile) as $line) {
-    echo $line. "\n"; #Check if first part of string is taken alredy
-    if($line != $_POST["username"]){
-        return true;
-    }
-    else{
-        echo "Username Taken";
-        return false;
-        break;
+    $username=$line.explode(":")
+    if(strtolower($username[0])==strtolower($_POST["username"])){
+
     }
  } 
 #---
