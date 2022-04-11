0a155f16738b1f93486ce9b71b39e870fd191379
Colson <103387364+borderlinecolson@users.noreply.github.com>
2022-04-11 22:02:15 +0200

    Login Button CSS

--- Pelican/Web/Signin.php
+++ Pelican/Web/Signin.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Pelican</title>
-  <link rel="stylesheet" href="css/Darkmode/signin.css">
-</head>
-<body>
+  <head>
+    <title>Pelican</title>
+    <link rel="stylesheet" href="css/Darkmode/signin.css">
+  </head>
 
-<form name="Signin" action="/Backend/Signin.php" method="post">
-  <input type="text" name="username" placeholder="Username" required>
-  <input type="password" name="username" placeholder="Password" required>
-  <input type="submit" value="Submit">
-</form>
-
+  <body>
+    <div class="form-container">
+      <form name="Signin" action="/Backend/Signin.php" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="username" placeholder="Password" required>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+  </body>
+</html>
  
