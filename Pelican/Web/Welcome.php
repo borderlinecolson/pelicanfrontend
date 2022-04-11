3dd1b3fe4d4d447bb5429bcbc687aa03ef11d82b
Colson <103387364+borderlinecolson@users.noreply.github.com>
2022-04-11 21:32:50 +0200

    updated welcome.php (css, structure)

--- Pelican/Web/Welcome.php
+++ Pelican/Web/Welcome.php
@@ -1,11 +1,23 @@
 <html>
   <head>
-    <link rel="stylesheet" href="css/Darkmode/index.css">
+    <link rel="stylesheet" href="css/Darkmode/welcome.css">
     <title>Pelican</title>
   </head>
 
   <body>
-    <h1 class="Motto">Its Time To Join Pelican</h1>
-    <h4 class="Motto">With the security of Matrix and the ease of Discord</h4>
-  </body>
+    <div class="container">
+      <header>
+        <h1 class="Motto">Its Time To Join Pelican</h1>
+        <h4 class="Motto">With the security of Matrix and the ease of Discord</h4>
+      </header>
+
+      <div>
+        <button onclick="login()">Login</button>
+      </div>
+    </div>
+      
+    <script>
+      const login = () => window.location.replace("Signin.php");
+    </script>
+    </body>
 </html>
