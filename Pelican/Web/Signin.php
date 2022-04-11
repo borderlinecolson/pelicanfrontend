02919dfa637946afe3254cb8293d04c469fe4964
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-11 19:04:12 -0400

    Update Signin.php

--- Pelican/Web/Signin.php
+++ Pelican/Web/Signin.php
@@ -1,25 +1,18 @@
+<!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="css/Darkmode/welcome.css">
     <title>Pelican</title>
+    <link rel="stylesheet" href="css/Darkmode/signin.css">
   </head>
 
   <body>
-    <div class="container">
-      <header>
-        <h1 class="Motto">Its Time To Join Pelican</h1>
-        <h4 class="Motto">With the security of Matrix and the ease of Discord</h4>
-      </header>
-
-      <div>
-        <button onclick="login()">Login</button>
-        <button onclick="register()">Register</button>
-      </div>
+    <div class="form-container">
+      <form name="Signin" action="/Backend/Signin.php" method="post" class="form-items">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <input type="submit" value="Submit">
+      </form>
     </div>
-      
-    <script>
-      const login = () => window.location.replace("Signin.php");
-      const regsiter = () => window.location.replace("Signup.php");
-    </script>
-    </body>
+  </body>
 </html>
+ 
